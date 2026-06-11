b5c90b2617cfdb51d3e9e7c78110c4ecb5315a01
alumno <50510940@ivt.edu.ar>
2026-06-11 10:42:26 -0300

    11 de junio

---
+++ TP5/app.js
@@ -0,0 +1,27 @@
+let numero1= 4
+let numero2= 15
+
+let suma 
+suma = numero1 + numero2
+console.log('El resultado de la suma es '+ suma)
+alert('La suma es:' + suma)
+
+let resta
+resta = numero2 - numero1
+console.log('El resulado de la resta es' + resta)
+alert('La resta es:' + resta)
+
+let multiplicacion 
+multiplicacion = numero2 * numero1
+console.log('El resulado de la multiplicacion es' + multiplicacion)
+alert('La multiplicacion es:' + multiplicacion)
+
+let division 
+division = numero1 / numero2
+console.log('El resulado de la division es' + division)
+alert('La division es:' + division)
+
+let nombre = 'Ambi'
+let apellido = 'Scelzi' 
+let edad = 15
+console.log('La alumna '+ nombre + ' ' + apellido + ' tiene '+ edad + ' años')
